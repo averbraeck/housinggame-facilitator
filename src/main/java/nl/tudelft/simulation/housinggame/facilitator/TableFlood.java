36e8fe2709c16542a02fd6dd7bacaef717dfcf6a
Alexander Verbraeck <a.verbraeck@gmail.com>
2024-02-23 13:54:22 +0100

    Copyright updated to 2024

--- src/main/java/nl/tudelft/simulation/housinggame/facilitator/TableFlood.java
+++ src/main/java/nl/tudelft/simulation/housinggame/facilitator/TableFlood.java
@@ -22,7 +22,7 @@ import nl.tudelft.simulation.housinggame.data.tables.records.PlayerRecord;
 /**
  * TableFlood makes and fills the flood information tables.
  * <p>
- * Copyright (c) 2020-2020 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * Copyright (c) 2020-2024 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license. See <a href="https://opentrafficsim.org/docs/current/license.html">OpenTrafficSim License</a>.
  * </p>
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
